004ec299dcbf91c65aea9541fe95901ebea092a7
Gavin Andresen <gavinandresen@gmail.com>
2020-02-12 14:58:54 -0500

    Configure triggerProcessing.js with a .env file
    I decided to keep this as simple as possible, so give a single
    WALLET_PRIVATE_KEY=... to specify which account pays for gas.

--- triggerProcessing.js
+++ triggerProcessing.js
@@ -4,43 +4,71 @@
  * Utility that subscribes to Thresher contract events on the chain
  * and calls processAll() if a bunch of blocks go by with entries
  * waiting to win/lose.
+ *
+ */
+/*
+ * Before running, create a .env configuration file; for example:
+
+BLOCKS_TO_WAIT=3
+WEB3_EVENT_PROVIDER=ws://localhost:9545
+WALLET_PRIVATE_KEY=348ce564d4.....
+TRACE=1
+
+ * ... and send some ETH to the address corresponding to WALLET_PRIVATE_KEY
  */
 
-const assert = require('assert')
-const Web3 = require('web3')
-const { toWei, fromWei } = require('web3-utils')
+const assert = require('assert');
+const Web3 = require('web3');
+const PrivateKeyProvider = require("truffle-privatekey-provider");
+const { toWei, fromWei } = require('web3-utils');
 
-/* How long to wait before calling Thresher.processAll */
-let BLOCKS_TO_WAIT = 3
+let web3;
+let accounts;
+let thresher;
 
-let web3
-let thresher
+let entriesWaiting = 0;
+let lastContributeBlock = 0;
 
-let entriesWaiting = 0
-let lastContributeBlock = 0
+require('dotenv').config();
 
 function addEntry(event) {
-    entriesWaiting = entriesWaiting + 1
-    lastContributeBlock = event.blockNumber
+    entriesWaiting = entriesWaiting + 1;
+    lastContributeBlock = event.blockNumber;
 }
 function removeEntry(event) {
-    entriesWaiting = entriesWaiting - 1
+    entriesWaiting = entriesWaiting - 1;
     if (entriesWaiting < 0) {
-        console.log("ERROR: Entries waiting : "+entriesWaiting)
-        entriesWaiting = 0
+        console.log("ERROR: Entries waiting : "+entriesWaiting);
+        entriesWaiting = 0;
     }
 }
 
 async function init() {
-    web3 = new Web3('ws://localhost:9545', null, { transactionConfirmationBlocks: 1 })
-    let contractJson = require('./build/contracts/Thresher.json')
+    if (process.env.WALLET_PRIVATE_KEY === undefined) {
+        console.log("You must give a WALLET_PRIVATE_KEY=a955... in the .env file");
+        process.exit(1);
+    }
 
-    let netId = await web3.eth.net.getId()
+    web3 = new Web3()
+    const eventProvider = new Web3.providers.WebsocketProvider(process.env.WEB3_EVENT_PROVIDER);
+    web3.setProvider(eventProvider)
+
+    const account = web3.eth.accounts.privateKeyToAccount('0x' + process.env.WALLET_PRIVATE_KEY);
+    web3.eth.accounts.wallet.add(account);
+    web3.eth.defaultAccount = account.address;
+    console.log(`Sending transcations (paying gas) from ${web3.eth.defaultAccount}`);
+
+    let contractJson = require('./build/contracts/Thresher.json');
+
+    let netId = await web3.eth.net.getId();
     if (contractJson.networks[netId]) {
-        const tx = await web3.eth.getTransaction(contractJson.networks[netId].transactionHash)
-        thresher = new web3.eth.Contract(contractJson.abi, contractJson.networks[netId].address)
-        thresher.deployedBlock = tx.blockNumber
-        thresher.options.from = (await web3.eth.getAccounts())[0]
+        const tx = await web3.eth.getTransaction(contractJson.networks[netId].transactionHash);
+        thresher = new web3.eth.Contract(contractJson.abi, contractJson.networks[netId].address);
+        thresher.deployedBlock = tx.blockNumber;
+        thresher.options.from = account.address;
+    } else {
+        console.log("Don't know where the contract is deployed on this network");
+        process.exit(1);
     }
 
     thresher.events.Contribute({
@@ -68,19 +96,22 @@ async function init() {
 
 async function processAll() {
     if (entriesWaiting == 0) {
-        return
+        return;
     }
     let currentBlock = await web3.eth.getBlockNumber();
 
-    if (currentBlock-lastContributeBlock >= BLOCKS_TO_WAIT) {
-        let g = await thresher.methods.processAll().estimateGas();
+    let blockGap = currentBlock-lastContributeBlock;
+    if (blockGap >= process.env.BLOCKS_TO_WAIT) {
+        if (process.env.TRACE) {
+            console.log(`${entriesWaiting } entries waited ${blockGap} blocks, calling processAll()`);
+        }
 
+        let g = await thresher.methods.processAll().estimateGas();
         await thresher.methods.processAll().send({gas: g});
     }
 }
 
-init()
+init();
 
-let looper = setInterval(processAll, 10*1000)
-looper.ref()  /* Referencing the timer makes node.js loop forever */
-
+let looper = setInterval(processAll, 10*1000);
+looper.ref();  /* Referencing the timer makes node.js loop forever */
